29fbe270c327583fb523405bc97e8034874f48e9
jlha65 <jose-luis1997@hotmail.com>
2017-03-04 15:41:06 -0600

    Coroutines

---
+++ Game Jam  2014/Assets/Scripts/CountdownText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownText : MonoBehaviour {
+	private float startTime;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameManager.instance.phase == 1)
+			gameObject.GetComponent<Text> ().text = "Time remaining: " + Mathf.Floor (GameManager.instance.time1 - Time.time);
+		else if (GameManager.instance.phase == 2)
+			gameObject.GetComponent<Text> ().text = "Time remaining: " + Mathf.Floor (GameManager.instance.time2 + (GameManager.instance.time1 - Time.time));
+		else if (GameManager.instance.phase == 3)
+			gameObject.GetComponent<Text> ().text = "Time remaining: " + Mathf.Floor (GameManager.instance.time3 + (GameManager.instance.time2 + (GameManager.instance.time1 - Time.time)));
+		else {
+			gameObject.GetComponent<Text> ().text = "Game ended!";
+		}
+	}
+}
--- Game Jam  2014/Assets/Scripts/GameManager.cs
+++ Game Jam  2014/Assets/Scripts/GameManager.cs
@@ -3,23 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+	public static GameManager instance { get; set;}
 	public GameObject [] players;
 	private Transform[] targets = new Transform[4];
+	public float time1, time2, time3;
+	public int phase;
+	private bool onPhase, gameFinished;
 	// Use this for initialization
 	void Start () {
-		
+		onPhase = false;
+		gameFinished = false;
+		phase = 1;
 		for (int i = 0; i < players.Length; i++) {
 			Instantiate(players[i], GameObject.FindWithTag("Spawn" + (i+1)).GetComponent<Transform>().position, Quaternion.identity);
 			players [i].GetComponent<Movement>().pNumber = i + 1;
 			targets[i] = players [i].GetComponent<Transform>();
 		}
-
+		StartCoroutine (Countdown (time1));
+		instance = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		for (int i = 0; i < targets.Length; i++) {
-			CameraControl.instance.m_Targets [i] = targets [i];
+		//for (int i = 0; i < targets.Length; i++) {
+			//CameraControl.instance.m_Targets [i] = targets [i];
+		//}
+		if (phase == 1 && !onPhase) {
+			StartCoroutine (Countdown (time1));
+		} else if (phase == 2 && !onPhase) {
+			StartCoroutine (Countdown (time2));
+		} else if (phase == 3 && !onPhase) {
+			StartCoroutine (Countdown (time3));
+		} else {
+			gameFinished = true;
 		}
+	}
+
+	public IEnumerator Countdown(float time){
+		print ("Started coroutine!");
+		onPhase = true;
+		yield return new WaitForSeconds (time);
+		phase++;
+		onPhase = false;
 	}
 }
