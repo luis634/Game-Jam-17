eb891e960296bc7f883b13407d348d2bf450b428
luiskrlosfr <lc.figueroar26@gmail.com>
2017-03-03 20:54:22 -0600

    Prueba 1 con cubo alb

---
+++ Game Jam  2014/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float fSpeed;
+	public float fJumpSpeed; 
+	public float fGravity;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= fSpeed;
+			if (Input.GetButton ("Jump")) {
+				moveDirection.y = fJumpSpeed;
+			}
+		}
+		moveDirection.y -= fGravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
