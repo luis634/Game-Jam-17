9072d5fd2b220d27827ec5c99fc16ed5b1a91997
luiskrlosfr <lc.figueroar26@gmail.com>
2017-03-04 00:54:14 -0600

    Animaciones completas knight

--- Game Jam  2014/Assets/Scripts/Movement.cs
+++ Game Jam  2014/Assets/Scripts/Movement.cs
@@ -7,18 +7,33 @@ public class Movement : MonoBehaviour {
 	public float fSpeed;
 	public float fJumpSpeed; 
 	public float fGravity;
+	public Animator anim;
 	public CharacterController cgController;
 	private Vector3 moveDirection = Vector3.zero;
 
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
+
 	void Update() {
-		CharacterController controller = GetComponent<CharacterController>();
-		if (cgController.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= fSpeed;
-			if (Input.GetButton ("Jump")) {
-				moveDirection.y = fJumpSpeed;
+		CharacterController controller = GetComponent<CharacterController>(); 
+		if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) { //Condicion para que la animacion
+			anim.SetFloat ("Speed", 1);												//cambie con el movimiento
+			if (cgController.isGrounded) {
+				moveDirection = new Vector3 (Input.GetAxis ("Vertical"), 0, Input.GetAxis ("Horizontal"));
+				moveDirection = transform.TransformDirection (moveDirection);
+				moveDirection *= fSpeed;
+				anim.SetFloat ("Jumpman", 0);
+			} else {
+				anim.SetFloat ("Jumpman", 1);
 			}
+		} else if(cgController.isGrounded){
+			anim.SetFloat ("Speed", 0); //Cambiar animacion de movimiento a estar parado
+		}else if (moveDirection.y != 0){
+				anim.SetFloat ("Jumpman", 1);
+		}
+		if (cgController.isGrounded && Input.GetButton ("Jump")) { //Salta solo si esta tocando el piso
+			moveDirection.y = fJumpSpeed;
 		}
 		moveDirection.y -= fGravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
